b3f334938fd3c069d5ce40e2a43d49296539b4dc
Duncan Tebbs <dtebbs@users.noreply.github.com>
2023-05-24 22:06:39 +0100

    linter configs and yarn commands

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -80,9 +80,9 @@ module.exports = {
       {
         version: '0.8.4'
       },
-      {
-        version: '0.5.16'
-      }
+      // {
+      //   version: '0.5.16'
+      // }
     ]
   },
   typechain: {
