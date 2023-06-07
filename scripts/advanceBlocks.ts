d1c298cccebede88ea596233085325f03d50436c
greatrat00 <>
2023-06-07 15:24:28 +0200

    fixing indentation

--- scripts/advanceBlocks.ts
+++ scripts/advanceBlocks.ts
@@ -1,9 +1,9 @@
 import { advanceBlocks } from "./utils/common";
 
 async function advance() {
-    // process.argv[2] will be the first command-line argument
-    const numBlocks = process.argv[2] ? parseInt(process.argv[2]) : 1;
-    await advanceBlocks(numBlocks);
+  // process.argv[2] will be the first command-line argument
+  const numBlocks = process.argv[2] ? parseInt(process.argv[2]) : 1;
+  await advanceBlocks(numBlocks);
 }
 
 advance();
